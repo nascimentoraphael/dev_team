34a646d2a9f7cdc23eefb305e33a819e06ab2aab
nasci <nascimentoraphael@yahoo.com.br>
2025-05-10 12:32:41 -0300

    chore: add /api/ping healthcheck

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
+// api/index.js
 const serverless = require('serverless-http');
 const app = require('../back-end/server');
+
+// <<< ADICIONE ISTO Logo Abaixo >>>
+app.get('/api/ping', (req, res) => {
+  return res.json({ pong: true });
+});
+
 module.exports = serverless(app);
